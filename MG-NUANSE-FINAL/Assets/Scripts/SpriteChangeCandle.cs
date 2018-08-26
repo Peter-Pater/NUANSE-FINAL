cb70487b12832c702d8cde3c5c99dcc67e493195
Xincheng Huang <xh721@nyu.edu>
2018-08-26 15:58:57 +0800

    Scene v1.0

--- MG-NUANSE-FINAL/Assets/Scripts/SpriteChangeCandle.cs
+++ MG-NUANSE-FINAL/Assets/Scripts/SpriteChangeCandle.cs
@@ -6,11 +6,18 @@ public class SpriteChangeCandle : MonoBehaviour {
 
     const int HALF_CANDLE = 1;
     const int BULLET = 2;
-    const int FULLCANDLE = 3;
+    const int ALMOST_CANDLE = 3;
+    const int FULLCANDLE = 4;
+    const int DEAD_CANDLE = 5;
 
     public Sprite halfCandle;
+    public Sprite almostCandle;
     public Sprite fullCandle;
+    public Sprite deadCandle;
+
     private int STATE = HALF_CANDLE;
+
+    public GameObject bullet;
 
 	// Use this for initialization
 	void Start () {
@@ -23,12 +30,24 @@ public class SpriteChangeCandle : MonoBehaviour {
         //Debug.Log(gameObject.name);
 
         if (STATE == HALF_CANDLE){
-            GameObject.Find("bullet").GetComponent<Renderer>().enabled = false;
+            bullet.SetActive(true);
             STATE = BULLET;
-        }
-
-        if (STATE == BULLET){
-            
+            GetComponent<BoxCollider2D>().offset = new Vector2(-0.06244355f, -0.9439293f);
+            GetComponent<BoxCollider2D>().size = new Vector2(1.850682f, 2.512366f);
+        }else if (STATE == BULLET){
+            GetComponent<SpriteRenderer>().sprite = almostCandle;
+            GetComponent<BoxCollider2D>().offset = new Vector2(-0.06244355f, -0.6454784f);
+            GetComponent<BoxCollider2D>().size = new Vector2(1.850682f, 3.109268f);
+            bullet.SetActive(false);
+            STATE = ALMOST_CANDLE;
+        }else if(STATE == ALMOST_CANDLE){
+            GetComponent<SpriteRenderer>().sprite = fullCandle;
+            GetComponent<BoxCollider2D>().offset = new Vector2(-0.06244355f, -0.3097228f);
+            GetComponent<BoxCollider2D>().size = new Vector2(1.850682f, 3.780779f);
+            STATE = FULLCANDLE;
+        }else if(STATE == FULLCANDLE){
+            GetComponent<SpriteRenderer>().sprite = deadCandle;
+            STATE = DEAD_CANDLE;
         }
 
     }
