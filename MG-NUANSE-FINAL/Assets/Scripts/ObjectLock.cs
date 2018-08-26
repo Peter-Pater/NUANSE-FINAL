7c74ae983e3684f9dd005dec4f61b15eb19c00b3
Sam Jianghao Hu <jh4828@nyu.edu>
2018-08-26 21:33:56 +0800

    Implemented solving order for scene 4

---
+++ MG-NUANSE-FINAL/Assets/Scripts/ObjectLock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectLock : MonoBehaviour {
+
+    public bool isLocked = true;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (isLocked){
+            GetComponent<Collider2D>().enabled = false;
+        }else{
+            GetComponent<Collider2D>().enabled = true;
+        }
+	}
+}
