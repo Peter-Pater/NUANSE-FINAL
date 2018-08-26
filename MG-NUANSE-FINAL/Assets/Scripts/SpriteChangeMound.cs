d0d1345f281350861f60cb22b22a54529c086d36
Xincheng Huang <xh721@nyu.edu>
2018-08-26 14:41:17 +0800

    Restore Scene 6

---
+++ MG-NUANSE-FINAL/Assets/Scripts/SpriteChangeMound.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChangeMound: MonoBehaviour {
+
+    const int MOUNDCLOSED = 1;
+    const int MOUNDOPEN = 2;
+
+    public Sprite mound_open;
+    public Sprite mound_closed;
+
+    private int STATE = MOUNDCLOSED;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = mound_closed;
+    }
+
+    private void OnMouseDown()
+    {
+
+        //Debug.Log(gameObject.name);
+
+        if (STATE == MOUNDCLOSED)
+        {
+            GetComponent<SpriteRenderer>().sprite = mound_open;
+            STATE = MOUNDOPEN;
+        }
+
+        if (STATE == MOUNDOPEN)
+        {
+            GameObject.Find("gun").GetComponent<Renderer>().enabled = true;
+            MouseDrag MouseDragScript;
+            MouseDragScript = GameObject.Find("gun").GetComponent<MouseDrag>();
+            MouseDragScript.canbedragged = true;
+            Debug.Log("drag now");
+        }
+
+    }
+	
+}
