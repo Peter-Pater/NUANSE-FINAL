15d737d2fc0b2390bb5c1187e15cfa29181bcd3a
Sam Jianghao Hu <jh4828@nyu.edu>
2018-08-28 01:42:06 +0800

    Added fireplace sound and wiping sound

---
+++ MG-NUANSE-FINAL/Assets/Scripts/Scene3/FirePlaceSound.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePlaceSound : MonoBehaviour {
+    
+    public Transform cameraTrans;
+    Transform thisScene;
+
+    bool isFireOn = true;
+    bool isFireSoundOn = false;
+
+
+    AudioSource fireAudioPlayer;
+
+
+	// Use this for initialization
+	void Start () {
+        thisScene = transform.parent.GetChild(0);
+        fireAudioPlayer = transform.GetChild(0).GetComponent<AudioSource>();
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+		
+        if (isFireOn && cameraTrans.position == thisScene.position){
+            if (!isFireSoundOn){
+                fireAudioPlayer.Play();
+                isFireSoundOn = true;
+            }
+        }else{
+            fireAudioPlayer.Stop();
+            isFireSoundOn = false;
+        }
+	}
+
+
+    private void OnMouseDown()
+    {
+        isFireOn = false;
+    }
+}
